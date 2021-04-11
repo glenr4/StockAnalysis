f37292152258e0e253e08bc94c4eef456dfcc571
Glen Rutherford <grutherford@ofigroup.com.au>
2021-04-11 10:19:59 +1000

    Added subplot of MA difference change

--- chart-ma.py
+++ chart-ma.py
@@ -1,13 +1,12 @@
 # https://medium.com/analytics-vidhya/using-plotly-for-building-candlestick-chart-for-the-stock-market-analysis-5305b48a5f75
+# Has now changed to use sub plots: https://plotly.com/python/multiple-axes/
 
-import pandas as pd
 import datetime as dt
 import pandas_datareader.data as web
-import plotly.express as px
-import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 # Data
-start = dt.datetime(2019,1,1)
+start = dt.datetime(2020,1,1)
 end=dt.datetime.now()
 stockSymbol = 'BTC-USD'
 # stockSymbol = 'ETH-USD'
@@ -16,38 +15,28 @@ stocks = web.DataReader([stockSymbol], 'yahoo', start, end)
 
 # Get the data for the stockSymbol and configure as hollow candlestick
 stockData = {'x': stocks.index,
-'open': stocks[('Open', stockSymbol)],
-'high': stocks[('High', stockSymbol)],
-'low': stocks[('Low', stockSymbol)],
-'close': stocks[('Close', stockSymbol)],
-'type': 'candlestick',
-'increasing':{'fillcolor': '#FFFFFF'},
-'decreasing':{'fillcolor': '#FF0000'}
-             }
+    'open': stocks[('Open', stockSymbol)],
+    'high': stocks[('High', stockSymbol)],
+    'low': stocks[('Low', stockSymbol)],
+    'close': stocks[('Close', stockSymbol)],
+    'type': 'candlestick',
+    'increasing':{'fillcolor': '#FFFFFF'},
+    'decreasing':{'fillcolor': '#FF0000'}
+ }
 
+# Moving averaged
 avg10 = stocks.Close[stockSymbol].rolling(window=10,min_periods=1).mean()
 avg50 = stocks.Close[stockSymbol].rolling(window=50, min_periods=1).mean()
 avg200 = stocks.Close[stockSymbol].rolling(window=200, min_periods=1).mean()
-diff = avg50 - avg200
+maDiff = avg50 - avg200
+maDiffChange = maDiff.diff()    # Difference from one row to the next
 
-
-# maDifference = {
-#     'x': stocks.index,
-#     'y': avg20,
-#     'type': 'scatter',
-#     'mode': 'lines',
-#     'line': {
-#         'width': 1,
-#         'color': 'blue'
-#     },
-#     'name': 'Moving Average 20'
-# }
-
+# Plot dictionaries
 maDifference = {
     'x': stocks.index,
-    'y': diff,
+    'y': maDiffChange,
     'type': 'bar',
-    'marker':{'color':'green'},
+    'marker':{'color':'rgba(0, 127, 14, 0.3)'},
     'name': 'Moving Average Difference'
 }
 
@@ -87,21 +76,24 @@ maLong = {
     'name': 'Moving Average 200'
 }
 
-# Chart
-fig = go.Figure(data=[stockData, maDifference, maShort, maMedium, maLong],
-                layout=go.Layout({
+# Chart with separate y-axes (secondary goes on top of primary)
+fig = make_subplots(specs=[[{"secondary_y": True}]])
+fig.add_trace(stockData, secondary_y=True)
+fig.add_trace(maShort, secondary_y=True)
+fig.add_trace(maMedium, secondary_y=True)
+fig.add_trace(maLong, secondary_y=True)
+
+fig.add_trace(maDifference, secondary_y=False)
+fig.update_layout({
     'title':{
         'text': stockSymbol,
         'font':{
             'size': 25
         }
-    },
-    "yaxis": {
-        "autorange": True,
-        "fixedrange": False,
-
     }
-}))
+})
+fig.update_yaxes(title_text=stockSymbol, secondary_y=True)
+fig.update_yaxes(title_text="EMA Difference Change", secondary_y=False)
 fig.show()
 
 
