5ef89973fe2e729df55ac7023be6d4e0a918ea24
Glen Rutherford <glenrutherford4@gmail.com>
2021-05-08 18:06:34 +1000

    Added trailing stop logic

--- chart-ma.py
+++ chart-ma.py
@@ -1,6 +1,8 @@
 # https://medium.com/analytics-vidhya/using-plotly-for-building-candlestick-chart-for-the-stock-market-analysis-5305b48a5f75
 # Has now changed to use sub plots: https://plotly.com/python/multiple-axes/
 
+# TODO: use this charting package instead, which has automatic y-axis scaling: https://github.com/highfestiva/finplot
+
 import datetime as dt
 import pandas_datareader.data as web
 from plotly.subplots import make_subplots
@@ -8,25 +10,32 @@ from IPython.display import display
 import pandas as pd
 import TimeSeriesResample as tsr
 import plotly.graph_objects as go
+import numpy as np
 
 # Settings
-start = dt.datetime(2018,7,26)   # dt.datetime(2019,1,1)
-tradeStart = dt.datetime(2017,7,26) # dt.datetime(2020,10,14) # equal to or after start
+start = dt.datetime(2010,1,1)   # dt.datetime(2019,1,1)
+tradeStart = dt.datetime(2011,1,1) # equal to or after start
 end=dt.datetime.now()
 
+# stockSymbol = 'SPY'
 # stockSymbol = 'BTC-USD'
+stockSymbol = 'TAN'
 # stockSymbol = 'TRX-USD'
-stockSymbol = 'ETH-USD'
+# stockSymbol = 'ETH-USD'
 # stockSymbol = 'XRP-USD'
 # stockSymbol = '^AXJO' # ASX200
 # stockSymbol = 'GC=F'  # Gold Jun 21 (GC=F)
 # stockSymbol = 'GLD'  # SPDR Gold Shares
+# stockSymbol = 'SYS-USD'
+# stockSymbol = '^TNX'    # US 10 year bond yield
+
 
 startingEquity = 100.0
 maShortPeriod = 10
 maMediumPeriod = 50
 maLongPeriod = 200
-timePeriod = 'daily' # daily, weekly or monthly
+timePeriod = 'weekly' # daily, weekly or monthly
+trailingStopPercent = 15.0
 
 # Get data
 stocks = web.DataReader(stockSymbol, 'yahoo', start, end)
@@ -36,6 +45,7 @@ if(timePeriod == 'monthly'):
     stocks = tsr.monthly(stocks)
 
 # Moving averages
+# TODO: use EMAs instead
 stocks['maShort'] = stocks['Close'].rolling(window=maShortPeriod,min_periods=maShortPeriod).mean()
 stocks['maMedium'] = stocks['Close'].rolling(window=maMediumPeriod, min_periods=maMediumPeriod).mean()
 stocks['maLong'] = stocks['Close'].rolling(window=maLongPeriod, min_periods=maLongPeriod).mean()
@@ -45,12 +55,49 @@ stocks['maDiffShortMedium'] = stocks['maShort'] - stocks['maMedium']
 stocks['maDiffShortMediumChange'] = stocks['maDiffShortMedium'].diff()    # Difference from one row to the next
 # display(stocks)
 
+
 # Calculate trade entry and exit points
-stocks['entryTrigger'] = (stocks['maShort'] > stocks['maMedium']) & (stocks['maMedium'] > stocks['maLong'])
-entryTriggerText = "(stocks['maShort'] > stocks['maMedium']) & (stocks['maMedium'] > stocks['maLong'])"
-stocks['exitTrigger'] = (stocks['maShort'] < stocks['maMedium'])
-exitTriggerText = "(stocks['maShort'] < stocks['maMedium'])"
-# display(stocks)
+stocks['entryTrigger1'] = (stocks['Close'] > stocks['maMedium'])
+stocks['entryTrigger2'] = (stocks['maMedium'] >= (stocks['maMedium'].shift(1)))
+
+stocks['entryTrigger'] = stocks['entryTrigger1'] & stocks['entryTrigger2']
+entryTriggerText = "stocks['Close'] > stocks['maMedium'] and increasing stocks['maMedium']"
+
+# Stop loss before is one possible exit, Trailing Stop gets applied after to give exitTrigger
+stocks['stopLoss'] = (stocks['Close'] < stocks['maMedium'])
+exitTriggerText = "stopLoss: stocks['Close'] < stocks['maMedium'] OR Trailing Stop {}%".format(trailingStopPercent)
+
+# Trailing Stop
+## To implement this, need to add a column to know when we are in a trade. The exitTrigger below
+# is now going to be the Stop Loss. Then we can calculate when we are in a trade, calculate the
+# Trailing Stop, then calculate the actual exitTrigger from either one signalling an exit.
+# Trailing Stop is key for getting out early when the price turns.
+# stocks['trailing_stop'] =
+# https://wiki.python.org/moin/BitwiseOperators
+stocks['inTrade'] = False
+stocks['inTrade'] = ((stocks['entryTrigger'].shift(1).fillna(False) | stocks['inTrade'].shift(1).fillna(False)) \
+                    & (~(stocks['stopLoss'].shift(1).fillna(False))))
+
+# Could not figure out a way to use vector operations to calculate Trailing Stop
+trailingStop = []
+lastTrailingStop = 0.0
+for row in stocks.iterrows():
+    data = row[1] # Note: row[0] is date time, row[1] is the row data
+
+    if data['inTrade']:
+        if data['High'] > lastTrailingStop:
+            trailingStop.append(data['High'])
+            lastTrailingStop = data['High']
+        else:
+            trailingStop.append(lastTrailingStop)
+    else:
+        trailingStop.append(0.0)
+        lastTrailingStop = 0.0
+
+stocks['trailingStop'] = np.asarray(trailingStop) * (100.0 - trailingStopPercent)/100.0
+
+# Exit Trigger
+stocks['exitTrigger'] = stocks['stopLoss'] | (stocks['Close'] < stocks['trailingStop'])
 
 # Calculate account equity
 # We are using the Close price for all indicators, therefore when entering or closing a trade,
@@ -190,16 +237,38 @@ maDiff2= {
     'name': 'Short-Medium Difference Change'
 }
 
+# maShort = {
+#     'x': stocks.index,
+#     'y': stocks['maShort'],
+#     'type': 'scatter',
+#     'mode': 'lines',
+#     'line': {
+#         'width': 2,
+#         'color': 'purple'
+#     },
+#     'name': 'Short Moving Average ({})'.format(maShortPeriod)
+# }
+# maShort = {
+#     'x': stocks.index,
+#     'y': stocks['inTrade'].astype(int)*50,
+#     'type': 'scatter',
+#     'mode': 'lines',
+#     'line': {
+#         'width': 2,
+#         'color': 'purple'
+#     },
+#     'name': 'inTrade'
+# }
 maShort = {
     'x': stocks.index,
-    'y': stocks['maShort'],
+    'y': stocks['trailingStop'],
     'type': 'scatter',
     'mode': 'lines',
     'line': {
         'width': 2,
         'color': 'purple'
     },
-    'name': 'Short Moving Average ({})'.format(maShortPeriod)
+    'name': 'trailingStop'
 }
 
 maMedium = {
@@ -213,6 +282,7 @@ maMedium = {
     },
     'name': 'Medium Moving Average ({})'.format(maMediumPeriod)
 }
+
 
 maLong = {
     'x': stocks.index,
